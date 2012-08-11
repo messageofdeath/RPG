05f1684331765d603fa9df1801563cfce79c9a9a
messageofdeath <messageofdeath@live.com>
2012-08-10 23:21:19 -0500

    Added MySQL Added Checks during boot up

--- src/me/messageofdeath/RPG/RPG.java
+++ src/me/messageofdeath/RPG/RPG.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import lib.PatPeter.SQLibrary.MySQL;
 import me.messageofdeath.RPG.API.Api;
 import me.messageofdeath.RPG.Commands.rpgCommand;
 import me.messageofdeath.RPG.Listeners.playerListener;
@@ -21,45 +22,54 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-
 public class RPG extends JavaPlugin {
 	
 	public static String lprefix;
-	public static File DBFile;
 	public static File CFile;
-	public static FileConfiguration db;
 	public static FileConfiguration config;
-	public static Economy economy = null;
+	public static Economy economy;
+	public static MySQL mysql;
 	
 	@Override
 	public void onEnable() {
 		PluginDescriptionFile pdfFile = getDescription();
 		PluginManager pm = getServer().getPluginManager();
-		Plugin citizens = pm.getPlugin("Citizens");
 		lprefix = "[" + pdfFile.getName() + "] v" + pdfFile.getVersion() + ": ";
+		log("is now enabling...");
+		log("is Citizens running?");
+		Plugin citizens = pm.getPlugin("Citizens");
 		if(citizens.isEnabled()) {
-			log("is now enabling...");
-			getWorldGuard();
+			log("Citizens is enabled. Continuing");
+			log("Registering Commands...");
 			getCommand("rpg").setExecutor(new rpgCommand());
+			log("Registered Commands!");
+			log("Registering Listeners...");
 			pm.registerEvents(new questListener(this), this);
 			pm.registerEvents(new playerListener(), this);
-			DBFile = new File(this.getDataFolder(), "database.yml");
+			log("Registered Listeners!");
+			log("Checking Config.yml");
 			CFile = new File(this.getDataFolder(), "config.yml");
 			try {firstRun();} catch (Exception e) {e.printStackTrace();}
 			config = new YamlConfiguration();
-			db = new YamlConfiguration();
 			loadYamls();
+			log("Config.yml loaded");
+			log("Hooking into MySQL!");
+			sqlConnection();
+	        sqlDoesDatabaseExist();
+	        log("is now connected to MySQL!");
+	        log("Hooking into Vault economy!");
 			setupEconomy();
+			log("Hooked into Vault economy!");
 			log("is now enabled!");
 		}else{
-			log("Citizens is now enabled! Shutting down...");
+			log("Citizens is not enabled! Shutting down...");
 			pm.disablePlugin(this);
 		}
 	} 
 	
 	@Override
 	public void onDisable() {
+		mysql.close();
 		log("is now disabled!");
 	}
 	
@@ -78,27 +88,37 @@ public class RPG extends JavaPlugin {
 	
 	@SuppressWarnings("static-access")
 	private void firstRun() throws Exception {
-		if (!this.DBFile.exists()) {
-			this.DBFile.getParentFile().mkdirs();
-			copy(getResource("database.yml"), this.DBFile);
-		}
 		if(!this.CFile.exists()) {
 			this.CFile.getParentFile().mkdirs();
 			copy(getResource("config.yml"), CFile);
 		}
 	}
-	
-	public WorldGuardPlugin getWorldGuard() {
-	    Plugin plugin = this.getServer().getPluginManager().getPlugin("WorldGuard");
-	    if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
-	    	this.getServer().getPluginManager().disablePlugin(Api.getPlugin());
-	    }
-	    return (WorldGuardPlugin) plugin;
+	public void sqlConnection() {
+		mysql = new MySQL(Bukkit.getLogger(), "[RPG]", config.getString("MySQL.Host"), config.getString("MySQL.Port")
+				, config.getString("MySQL.Database"), config.getString("MySQL.User"), config.getString("MySQL.Password"));
+		mysql.open();
 	}
+	
+	public void sqlDoesDatabaseExist() {
+		if(!mysql.checkTable("Quests")) {
+			log("Error: The Table Quests does not exist creating...");
+			mysql.query("CREATE TABLE Quests (NpcId INT PRIMARY KEY, QuestName VARCHAR(100), QuestObjective VARCHAR(300), QuestId INT, QuestType VARCHAR(50), RewardItem VARCHAR(100), RewardMoney DOUBLE, RequirementItem VARCHAR(100), RequiredQuest INT);");
+			log("The Table Quests has been created!");
+		}
+		if(!mysql.checkTable("Quests_Users")) {
+			log("Error: The Table Quests_Users does not exist creating...");
+			mysql.query("CREATE TABLE Quests_Users (Name VARCHAR(50), ActiveQuest INT, PendingQuest INT, CompletedQuests VARCHAR(600), Quest VARCHAR(100));");
+			log("The Table Quests_Users has been created!");
+		}
+		if(!mysql.checkTable("Quests_Buttons")) {
+			log("Error: The Table Quests_Buttons does not exist creating...");
+			mysql.query("CREATE TABLE Quests_Buttons (Name VARCHAR(100), Message VARCHAR(150), ButtonLocation VARCHAR(50), Location VARCHAR(100));");
+			log("The Table Quests_Buttons has been created!");
+		}
+    }
 	
 	public void loadYamls() {
 		try {
-			db.load(DBFile);
 			config.load(CFile);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -107,7 +127,6 @@ public class RPG extends JavaPlugin {
 	
 	public void saveYamls() {
 		try {
-			db.save(DBFile);
 			config.save(CFile);
 		} catch (IOException e) {
 			e.printStackTrace();

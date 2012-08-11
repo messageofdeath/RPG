bc5c8e3f4320296cbafe56c1992c9723cc245a3b
messageofdeath <messageofdeath@live.com>
2012-08-10 23:20:22 -0500

    MySQL

--- src/me/messageofdeath/RPG/API/Button.java
+++ src/me/messageofdeath/RPG/API/Button.java
@@ -1,5 +1,8 @@
 package me.messageofdeath.RPG.API;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Button {
 	
 	private String location;
@@ -7,14 +10,56 @@ public class Button {
 		location = loc;
 	}
 	// Locations
-	public String getButtonLocation() {return Api.getConfig().getString("ButtonLocations." + location + ".Location");}
-	public String getButtonName() {return Api.getConfig().getString("ButtonLocations." + location + ".Name");}
-	public String getButtonMessage() {return Api.getConfig().getString("ButtonLocations." + location + ".Message");}
+	public String getButtonLocation() {
+		ResultSet rs = Api.getMySQL().query("Select * FROM Quests_Buttons WHERE ButtonLocation = '"+location+"'");
+		try {
+			if(rs.first()) {
+				return rs.getString("Location");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public String getButtonName() {
+		ResultSet rs = Api.getMySQL().query("Select * FROM Quests_Buttons WHERE ButtonLocation = '"+location+"'");
+		try {
+			if(rs.first()) {
+				return rs.getString("Name");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public String getButtonMessage() {
+		ResultSet rs = Api.getMySQL().query("Select * FROM Quests_Buttons WHERE ButtonLocation = '"+location+"'");
+		try {
+			if(rs.first()) {
+				return rs.getString("Message");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 		
-	public void setButtonLocation(String location) {Api.getConfig().set("ButtonLocations." + this.location + ".Location", location);}
-	public void setButtonName(String name) {Api.getConfig().set("ButtonLocations." + location + ".Name", name);}
-	public void setButtonMessage(String message) {Api.getConfig().set("ButtonLocations." + location + ".Message", message);}
+	public void setButtonLocation(String location) {
+		Api.getMySQL().query("UPDATE Quests_Buttons SET Location = '"+location+"' WHERE ButtonLocation = "+this.location);
+	}
+	public void setButtonName(String name) {
+		Api.getMySQL().query("UPDATE Quests_Buttons SET Name = '"+name+"' WHERE ButtonLocation = "+this.location);
+	}
+	public void setButtonMessage(String message) {
+		Api.getMySQL().query("UPDATE Quests_Buttons SET Message = '"+message+"' WHERE ButtonLocation = "+this.location);
+	}
 	
-	// Save
-	public void save() {Api.saveConfig();}
+	public void deleteButton() {
+		Api.getMySQL().query("DELETE FROM Quests_Buttons WHERE ButtonLocation= '"+location+"'");
+	}
+	
+	public void addButton(String name, String message, String buttonlocation, String location) {
+		name = name.replace("_", " ");
+		Api.getMySQL().query("INSERT INTO Quests_Buttons (Name, Message, ButtonLocation, Location) VALUES ('"+name+"','"+message+"','"+buttonlocation+"','"+location+"')");
+	}
 }
